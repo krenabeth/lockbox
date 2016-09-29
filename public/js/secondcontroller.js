eded6f00da081ba0a221dd8ada219ada58cc10bb
Karen Langner <krenabeth@yahoo.com>
2016-09-28 22:04:26 -0400

    beginning phone number

--- public/js/secondcontroller.js
+++ public/js/secondcontroller.js
@@ -1,5 +1,25 @@
 var app = angular.module('lockApp');
 
 app.controller('secondCtrl', ['$scope', '$location', function($scope, $location){
-	console.log('Howdy');
+
+	$scope.today = function date() {
+		var date = Date.now();
+		console.log(date);
+		return date;
+	};
+	
+		$scope.dial = function(num) {
+		// check all nine numbers
+		console.log('Howdy');
+
+		console.log(num);
+		if(num === 2) {
+			window.alert('Congratulations!');
+			// change path to lock3
+			$location.path('/lock3');	
+		} else {
+			window.alert('Nope, sorry. Hint: You could call home for a clue... try again!');
+		}
+
+	};
 }]);
