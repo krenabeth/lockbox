5e7d4c47b1bbc89b0f1e59c125ddb4f5077b0b22
Karen Langner <krenabeth@yahoo.com>
2016-09-29 16:23:05 -0400

    checks correct number match with user info

--- public/js/secondcontroller.js
+++ public/js/secondcontroller.js
@@ -1,6 +1,10 @@
 var app = angular.module('lockApp');
 
-app.controller('secondCtrl', ['$scope', '$location', function($scope, $location){
+app.controller('secondCtrl', ['$scope', '$http', '$location', function($scope, $http, $location){
+
+	$http.get('../userinfo/user.json').success(function(data) {
+		$scope.user = data;
+	});
 
 	$scope.today = function date() {
 		var date = Date.now();
@@ -8,18 +12,33 @@ app.controller('secondCtrl', ['$scope', '$location', function($scope, $location)
 		return date;
 	};
 	
-		$scope.dial = function(num) {
-		// check all nine numbers
-		console.log('Howdy');
+	var phone = [];
 
-		console.log(num);
-		if(num === 2) {
-			window.alert('Congratulations!');
-			// change path to lock3
-			$location.path('/lock3');	
-		} else {
-			window.alert('Nope, sorry. Hint: You could call home for a clue... try again!');
+	$scope.dial = function(num) {
+		
+		phone.push(num);
+		var hooray = false;
+
+		// check all nine numbers
+		if (phone.length === 10) {
+			console.log(phone);
+
+			for (var i = 0; i < 10; i++) {
+				if (phone[i] !== user.Phone[i]) {
+					window.alert('Nope, sorry. Hint: You could call home for a clue... try again!');
+					return phone = [];
+				} else {
+					hooray = true;
+				}
+
+			}
+			if (hooray) {
+				window.alert('Congratulations!');
+				// change path to lock3
+				$location.path('/lock3');
+			}
 		}
+
 
 	};
 }]);
